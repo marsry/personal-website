24aa56efdb6ff9e55b3478070edd7a1e260d01ea
Marsry Jan Sevilla <Marsry Jan Sevilla>
2023-09-01 17:47:05 +0800

    feat: homepage sections

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,16 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
-import Button from "@mui/material/Button";
-import ContainedButton from "./ContainedButton";
-import TextButton from "./TextButton";
+import ContainedButton from "./buttons/ContainedButton";
+import TextButton from "./buttons/TextButton";
 
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed" sx={{ backgroundColor: "#141A1F" }}>
+      <AppBar
+        position="fixed"
+        sx={{ backgroundColor: "rgba(20, 26, 31, 0.8)" }}
+      >
         <Toolbar
           sx={{
             display: "flex",
@@ -27,12 +29,12 @@ export default function ButtonAppBar() {
           >
             Logo
           </IconButton>
-          <div>
+          {/* <div>
             <TextButton name="About me" color="light" />
             <TextButton name="Experience" color="light" />
             <TextButton name="Portfolio" color="light" />
-          </div>
-          <ContainedButton name="Contact Me" />
+          </div> */}
+          <ContainedButton name="Contact me" />
         </Toolbar>
       </AppBar>
     </Box>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,86 @@
-import React, { Component } from "react";
+import React from "react";
+import headerImage from "../assets/header-pic.webp";
+import { Grid, Stack, Typography } from "@mui/material";
+import { TypeAnimation } from "react-type-animation";
 
-export default class Homepage extends Component {
-  render() {
-    return <div>Hello</div>;
-  }
+export default function Homepage() {
+  return (
+    <Stack alignItems="center" justifyContent="center">
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+        sx={{
+          height: "100vh",
+          width: "90vw",
+          backgroundImage: `url(${headerImage})`,
+          backgroundPosition: "center",
+          marginTop: "20px",
+        }}
+      >
+        <Grid item xs={12} sm={6}>
+          <Typography fontSize={64} fontWeight={600}>
+            Designer
+          </Typography>
+          <Typography fontSize={24} sx={{ color: "#7388A4" }}>
+            UI/UX designer
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Typography fontSize={64} fontWeight={600}>
+            Developer
+          </Typography>
+          <Typography fontSize={24} sx={{ color: "#7388A4" }}>
+            Frontend developer
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          border: "1px solid #7388A4",
+          paddingX: "150px",
+          paddingY: "50px",
+          marginBottom: "20px",
+          backgroundColor: "#152029",
+          //   borderRadius: "10px",
+        }}
+      >
+        <Grid item xs={12} sm={5} align="left">
+          <TypeAnimation
+            sequence={["", 1000, "Hello!", 500, "Hello! I'm Marsry!", 10000]}
+            wrapper="span"
+            cursor={true}
+            repeat={Infinity}
+            style={{
+              fontSize: "48px",
+              fontWeight: 600,
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={7}>
+          <Typography
+            fontSize={24}
+            fontWeight={400}
+            align="left"
+            sx={{ color: "#0074E8" }}
+          >
+            About me
+          </Typography>
+          <Typography fontSize={18} align="left">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Typography>
+        </Grid>
+      </Grid>
+    </Stack>
+  );
 }
